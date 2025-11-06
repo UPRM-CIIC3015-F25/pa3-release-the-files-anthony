2650ea82c69114faf84403a51eddae4146096adf
HGab13 <gabriellopezcaro13@gmail.com>
2025-11-06 16:59:48 -0400

    Implement createDeck (TASK 1)

--- Deck/DeckManager.py
+++ Deck/DeckManager.py
@@ -138,14 +138,19 @@ class DeckManager:
         return jokers
 
     # ---------- Deck creation ----------
-    # TODO (TASK 1): Implement a function that creates a full deck of Cards.
+    # DONE? (TASK 1): Implement a function that creates a full deck of Cards.
     #   Loop through all possible suits and ranks, retrieve the corresponding image
     #   from the card_images dictionary using (suit, rank) as the key, and create a Card
     #   object for each valid combination. If a matching image is not found, skip that card.
     #   Add each created Card to a list called 'deck' and return the completed list at the end.
+    # TODO: Check if works with rest of game
     def createDeck(self, subLevel: SubLevel = None):
         cardImages = self.load_card_images(subLevel)
         deck = []
+        for card in cardImages:
+            suit, rank = card
+            if cardImages[card]:
+                deck.append(Card(suit=suit, rank=rank, image=cardImages[card]))
         return deck
 
     # TODO (TASK 5.1): Complete the priceMap variable by assigning each joker a price.
