0b0e78f50e626605765d56e566065a333b545683
NuclearVM <asteroiddestroyer5709@gmail.com>
2025-11-24 11:43:32 -0400

    Added Tarot dictionary (Descriptions need to be changed still)

--- Cards/Tarots.py
+++ Cards/Tarots.py
@@ -11,3 +11,17 @@ class TarotCard:
 
     def sellPrice(self):
         return int(self.price * 0.6)
+
+Tarots = {
+    "Silver Chariot": TarotCard("Silver Chariot", "levels up High Card", 6),
+    "Magician's Red": TarotCard("Magician's Red", "levels up One Pair", 6),
+    "Star Platinum": TarotCard("Star Platinum", "levels up Two Pair", 6),
+    "Hierophant Green": TarotCard("Hierophant Green", "levels up Three of a Kind", 6),
+    "Justice": TarotCard("Justice", "levels up Straight", 6),
+    "Judgment": TarotCard("Judgment", "Creates a random Joker card", 6),
+    "The Fool": TarotCard("The Fool", "levels up Full House", 6),
+    "The Emperor": TarotCard("The Emperor", "levels up Four of a Kind", 6),
+    "The Hanged Man": TarotCard("The Hanged Man", "levels up all hands", 6),
+    "Death 13": TarotCard("Death 13", "levels up all hands", 6),
+    "The World": TarotCard("The World", "levels up all hands", 6)
+}
